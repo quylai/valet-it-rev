3c7336e01b8223cbda78ffa861f008c1795184d8
quyl <quylai@gmail.com>
2019-09-26 10:14:50 -0500

    retry

--- server.js
+++ server.js
@@ -35,16 +35,30 @@ newUserRoutes(app);
 import authRoutes from './routes/auth';
 authRoutes(app);
 
+//---------------------------------------------------------------------------------
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+//---------------------------------------------------------------------------------
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+//---------------------------------------------------------------------------------
+
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
